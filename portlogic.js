9ebb90de126c58452c1d758a0c074fe0b8b46184
thorblock <135666868+thorblock@users.noreply.github.com>
2023-10-02 21:06:08 -0700

    heat island set

--- portlogic.js
+++ portlogic.js
@@ -10,12 +10,58 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // store the fetch result as a variable so that we can manipulate backend
-var geoJSONzip = null;
+var geoJSONzip;
+
+// brandish style object before var call
+var customStyle = {
+    color: 'purple', // border color
+    fillColor: 'grey',  
+    weight: 1.5,         // border width
+    opacity: 0.8,        // border opacity
+    fillOpacity: 0.2   // fill opacity
+};
 
 // read and display local zip geojson
 fetch("/data/ZIP_Codes_Portland.geojson")
     .then((response) => response.json())
     .then((data) => {
-        L.geoJSON(data).addTo(map);
+        // reference declaration
+        geoJSONzip = data;
+        // create geoJson layer, implement custom style created beforehand
+        L.geoJSON(geoJSONzip, {
+            // style goes here
+            style: customStyle
+        }).addTo(map);
         });
 
+// ------------------
+// now let's try adding 
+// create and store second var
+var geoJSONheat;
+
+// manifest custom style beforehand
+var customStyle2 = {
+    color: 'red', // border color
+    fillColor: 'orange',  
+    weight: 1,         // border width
+    opacity: 0.8,        // border opacity
+    fillOpacity: 0.2   // fill opacity
+};
+
+//fetch and display
+fetch("/data/Urban_Heat_Island_Mapping.geojson")
+    .then((response) => response.json())
+    .then((data) => {
+        // reference declaration
+        geoJSONheat = data;
+
+        // second layer group var
+        heatLayer = L.layerGroup();
+
+        // create geoJson layer, implement custom style created beforehand
+        L.geoJSON(geoJSONheat, {
+            // style goes here
+            style: customStyle2
+        }).addTo(map);
+        });
+// ------------------
